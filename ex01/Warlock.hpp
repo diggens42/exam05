f4ba7593a642ff38e6cd3ecc88fa7f6476cf11b1
diggens42 <frederic.marcel.wahl@gmail.com>
2024-12-15 23:34:28 +0100

    ex01

---
+++ ex01/Warlock.hpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include "ASpell.hpp"
+
+class Warlock
+{
+	public:
+		Warlock(const std::string& name, const std::string& title);
+		~Warlock();
+		std::string const & getName() const;
+		std::string const & getTitle() const;
+		void	setTitle(std::string const & str);
+		void	introduce() const;
+		void	learnSpell(ASpell* spell);
+		void	forgetSpell(std::string spellname);
+		void	launchSpell(std::string spellname, ATarget const & target);
+
+
+	private:
+		Warlock & operator=(Warlock const & rhs);
+		Warlock(Warlock const & obj);
+		Warlock();
+		std::string	_name;
+		std::string	_title;
+		std::map<std::string, ASpell*> _spellBook;
+
+};
